3c1f93b6d59ed502f8680b9458cce68612831bdb
Patrycja Kucharska <kucharskap@gmail.com>
2026-03-23 16:14:28 +0100

    layout base

--- custom/my-layout.tsx
+++ custom/my-layout.tsx
@@ -1,7 +1,11 @@
 import type { CustomCVProps } from "cv-pdf-gen";
 
+
 export default function MyLayoutCV({ content, photoUrl }: CustomCVProps) {
   const { personal, experience, technical_skills, soft_skills, education, languages, labels } = content;
+  const website = content("personal.contact.website") as string | undefined;
 
-  return (<></>)
+  return (
+    <></>
+  )
 }
